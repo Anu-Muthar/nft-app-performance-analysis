b03bf2ea552d2d72664da206bf49eeb538c28e7a
Anu-Muthar <anusree2875@gmail.com>
2025-05-14 19:36:24 +0530

    Update sql_queries.sql

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,1 +1,16 @@
+-- 1. Monthly Revenue Trend
+SELECT DATE_TRUNC('month', transaction_date) AS month,
+       SUM(price) AS total_revenue
+FROM nft_transactions
+WHERE transaction_type = 'purchase'
+GROUP BY month
+ORDER BY month;
+
+-- 2. Top NFT Categories
+SELECT n.category, COUNT(t.transaction_id) AS sales_count, SUM(t.price) AS revenue
+FROM nft_transactions t
+JOIN nfts n ON t.nft_id = n.nft_id
+WHERE t.transaction_type = 'purchase'
+GROUP BY n.category
+ORDER BY revenue DESC;
 
